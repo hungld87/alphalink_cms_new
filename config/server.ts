813e01a5150147e78ef8b89f4c4fa1c01dae2fc3
Nguyễn Thắng <nguyenthang12091999@gmail.com>
2025-12-11 22:34:06 +0700

    udpate url

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,9 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', ''),
   app: {
     keys: env.array('APP_KEYS'),
   },
+  proxy: true, // Trust proxy headers from Nginx
 });
